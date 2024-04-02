d13bf39f07e659860559d7c97e551760ba76b6f3
aanvarjon <146158413+aanvarjon@users.noreply.github.com>
2024-04-02 21:17:56 +0600

    Add files via upload

---
+++ Новая папка/index.js
@@ -0,0 +1,30 @@
+// 1. Arrayning filter methodini vazifasini bajaradigan funksiya yozing. Callback orqali 
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const myFunc = (a, b) => {
+//     return a,b % 2 !==0;
+// };
+
+// const filteredArr = arr.filter(myFunc);
+// console.log(filteredArr);
+
+// 2. Arrayning find methodini vazifasini bajaradigan funksiya yozing. Callback orqali
+
+//  find metodi massivda topilgan birinchi elementning qiymatini qaytaradi
+
+// const numbers = [2, 8, 13, 10, 9];
+
+// function chekEvent(number) {
+//     return number % 3 ===0;
+// }
+
+// const eventNumber = numbers.find(chekEvent);
+
+// console.log(eventNumber);
+
+// 3. Foydalanuvchi son kiritsin va shuncha soniyadan keyin "Hello World" textini chiqarib bersin.
+
+// let son = prompt("Son kiriting:");
+//         setTimeout(() => {
+//             console.log("Hello World");
+//         }, son * 1000);
